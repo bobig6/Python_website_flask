45970bb35cbbdc0e83bcdd93e45aa6c99638758f
spiritomtom <spiritomtom>
2020-01-22 20:23:42 +0200

    Real changes in user functions,because there was a mistake in my ordering

--- usser.py
+++ usser.py
@@ -17,50 +17,61 @@ from errors import ApplicationError
 app = Flask(__name__)
 
 class User:
-    def __init__(self,user_id,email,password,name,address,phone_number):
-        self.user_id=user_id
-        self.email=email
-        self.password=password
-	self.name=name
-        self.address=address
-        self.phone_number=phone_number
-    
+    def __init__(self,id,email,password,nama,address,phone_number):
+		self.id=id
+		self.email=email
+		self.password=password
+		self.nama=nama
+		self.address=address
+		self.phone_number=phone_number
+
     @staticmethod
-    def create_user(user_id, email,password,name, address, phone_number):
+    def create_user(id, email,password,nama, address, phone_number):
         result = None
         with SQLite() as db:
-            result = db.execute("INSERT INTO user (username, password, email, address, phone_number) VALUES (?, ?, ?, ?, ?)",
-                    (user_id,email, password,name, address, phone_number))
+            result = db.execute("INSERT INTO user (id, email, password,nama, address, phone_number) VALUES (?, ?, ?, ?, ?, ?)",
+                    (id,email, password,nama, address, phone_number))
         if result.rowcount == 0:
             raise ApplicationError("No value present", 404)
             
     @staticmethod
-    def find(user_id):
+    def find(id):
         result = None
         with SQLite() as db:
             result = db.execute(
                     "SELECT username, password, id FROM user WHERE id = ?",
-                    (user_id,))
+                    (id,))
         user = result.fetchone()
         if user is None:
             raise ApplicationError(
-                    "Post with id {} not found".format(user_id), 404)
+                    "Post with id {} not found".format(id), 404)
         return User(*user)
 
     
-    @staticmethod
-    def all():
-        with SQLite() as db:
-            result = db.execute(
-                    "SELECT user_id,email, password,name,address,phone_number FROM user").fetchall()
-            return [User(*row) for row in result]
+	
+
+	@staticmethod
+	def edit(part_you_wish_to_edit):
+		part_you_want_to_edit=raw_input("Enter the part you wish to edit:" )
+	if part_you_wish_to_edit=="email" or part_you_wish_to_edit=="password":
+		raise ApplicationError("You can't change your email or password",404)
+	elif part_you_wish_to_edit=="nama":
+		new_name=raw_input("Enter the nama you want:" )
+		User.nama=new_name
+	elif part_you_wish_to_edit=="address":
+		new_address=raw_input("Enter your new address:" )
+		User.address=new_address
+	elif part_you_want_to_edit=="phone number":
+		new_phone_number=raw_input("Enter your new number:")
+		User.phone_number=new_phone_number
+
 
     @staticmethod
-    def delete(user_id):
+    def delete(id):
         result = None
         with SQLite() as db:
             result = db.execute("DELETE FROM user WHERE id = ?",
-                    (user_id,))
+                    (id,))
         if result.rowcount == 0:
             raise ApplicationError("No value present", 404)
 
@@ -72,12 +83,9 @@ class User:
 
 
 #zasto mi kazva che podavam 6 argumenta kato se iskat 5(a te realno se iskat 6), a kato iztriq nqkoi ot argumentite mi kazva podal si 5 iskat se 6 ???????
-
-	
+#dali edit raboti vupreki hamalskoto izpulnenie
+#	
 
 Petko = User.create_user("33","p.dapchev09@abv.bg", generate_password_hash("pepi"), "Petko Dapchev", "Sofia","08")
 print(User.find_by_id("33").id)
-
-
-
 
